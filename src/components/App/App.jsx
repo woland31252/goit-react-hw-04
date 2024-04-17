b255e8e9156d8761d3d3df90242965eead3d000b
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-04-18 01:55:50 +0300

    fix Yup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import { fetchImages }  from '../../image_api.js';
 import SearchBar from '../SearchBar/SearchBar.jsx';
 import ImageGallery from '../ImageGallery/ImageGallery.jsx';
@@ -9,6 +10,8 @@ export default function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(false);
+
+const notify = () => toast('Please formulate your request.');
 
   const handleSearch = async (query) => {
       try {
@@ -30,7 +33,8 @@ export default function App() {
       <div className={css.container}>
         {isLoading && <Loader />}
         {error && <p>Do not find images</p>}
-        {images.length>0 && <ImageGallery collection={ images} />} 
+        {images.length > 0 && <ImageGallery collection={images} />} 
+        <Toaster toasty={notify} />
       </div>
     </>
     
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,30 @@
-import { Field, Form, Formik } from "formik"
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import * as Yup from 'yup';
 import { SlMagnifier } from "react-icons/sl";
 import { useId } from "react";
-import css from '../SearchBar/SearchBar.module.css'
+import css from '../SearchBar/SearchBar.module.css';
 
-export default function SearchBar({ onSearch }) {
+export default function SearchBar({ onSearch, toasty }) {
+
+    const InputSchema = Yup.object().shape({
+        query: Yup.string().required(toasty),
+        
+    });
     const id = useId();
+    
     return (
 
         <Formik initialValues={{ query: "" }}
                     onSubmit={(values, actions) => {
                         onSearch(values.query);
-                        actions.resetForm();
-                }}>
+                        actions.resetForm();}}
+            validationSchema={InputSchema}>
+            
             <header className={css.headerCont}>
                 <Form className={css.form}>
-                    <Field className={css.input} type="text" id={id} name="query" autoComplete="off" autoFocus placeholder="Search images and photos"/>
-                    <button className={css.button} type="submit"><SlMagnifier className={ css.icon} /></button>
+                    <Field className={css.input} type="text" id={id} name="query" autoComplete="off" autoFocus placeholder="Search images and photos" />
+                    <ErrorMessage name="guery"/>
+                    <button className={css.button} type="submit"><SlMagnifier className={css.icon} /></button>
                 </Form>
             </header>
         </Formik>
